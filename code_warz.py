4722130286fb6eac1ae08714947eb0aa906c61ab
Rustamjon <rustamjon.18122006@gmail.com>
2025-06-20 10:57:56 +0500

    second_commit

---
+++ code_warz.py
@@ -0,0 +1,30 @@
+#1
+"""
+def solution(a, b):
+    return a+b+a if len(b) > len(a) else b+a+b
+"""
+
+#2
+"""
+def correct(s):
+    return s.replace('5', 'S').replace('0', 'O').replace('1', 'I')
+"""
+
+
+#3
+"""
+def fake_bin(x):
+    return "".join(["0" if int(xi) < 5 else "1" for xi in x])
+"""
+
+#4
+"""
+def abbrev_name(name):
+    return f"{name.title().split()[0][0]}.{name.title().split()[1][0]}"
+"""
+
+#5
+"""
+def abbrev_name(name):
+    return f"{name.title().split()[0][0]}.{name.title().split()[1][0]}"
+"""
